fc71e504f4fa2e19aaec3eda629e6edf6c50bffc
muhammedmanduz <muhammed.manduz@gmail.com>
2025-05-20 11:36:43 +0300

    React ve tailwind css kurulumu yapıldı

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 
+// MongoDB bağlantısı
 const db = () => {
   mongoose
     .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/yourdbname")
---
+++ frontend/src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div className="text-center text-amber-700 my-10">App</div>;
+};
+
+export default App;
